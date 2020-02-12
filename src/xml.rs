e385ef666d5747592d337090304f2915ee8d3a31
Stefano Montesi <dincio.montesi@gmail.com>
2020-02-12 10:38:29 +0100

    Implement crappy version of `xml::tree` parser

--- src/xml.rs
+++ src/xml.rs
@@ -11,38 +11,116 @@ pub struct Element {
     children: Vec<Element>,
 }
 
-/// Match XML element into dedicated structure (`Element`)
-pub fn xml_ele(input: &str) -> ParseResult<'_, Element> {
-    // Support parsers. Their name indicate what they parse
-    let attr_pair = pair(identifier, right(literal("="), quoted_string()));
+/// Match xml element argument along with its value
+///
+/// e.g. `hello="there"`
+fn attr_pair<'a>() -> impl Parser<'a, (String, String)> {
+    pair(identifier, right(literal("="), quoted_string()))
+}
 
-    let attr_lst = zero_or_more(right(space1(), attr_pair));
+/// Match xml element argument list
+fn attr_list<'a>() -> impl Parser<'a, Vec<(String, String)>> {
+    zero_or_more(right(space1(), attr_pair()))
+}
 
-    let ele = right(
-        literal("<"),
-        pair(identifier, left(attr_lst, literal("\\>"))),
-    );
+/// Utility that matches internal structures of element
+fn ele_internals<'a>() -> impl Parser<'a, Element> {
+    let raw_parser = pair(identifier, attr_list());
 
-    map(ele, |(name, attributes)| Element {
+    raw_parser.map(|(name, attributes)| Element {
         name,
         attributes,
         ..Default::default()
     })
-    .parse(input)
 }
 
-pub fn xml_tree(_input: &str) -> ParseResult<Element> {
-    unimplemented!()
+/// Match XML opening element
+///
+/// e.g. in `<hello-there arg="val" arg2="val2"> ... <\hello-there>` the following is matched:
+/// `<hello-there arg=...>`.
+fn open_ele<'a>() -> impl Parser<'a, Element> {
+    sorround(literal("<"), ele_internals(), literal(">"))
+}
+
+/// Match XML closing element
+///
+/// e.g. as in `<\closing>`
+fn close_ele<'a>() -> impl Parser<'a, Element> {
+    sorround(literal("<\\"), ele_internals(), literal(">"))
+}
+
+/// Match XML element into dedicated structure (`Element`)
+pub fn single_ele<'a>() -> impl Parser<'a, Element> {
+    sorround(literal("<"), ele_internals(), literal("\\>"))
+}
+
+/// Match XML tree
+pub fn tree(input: &str) -> ParseResult<Element> {
+    // Try to match single element
+    if let ok @ Ok(_) = single_ele().parse(input) {
+        return ok;
+    }
+
+    // Match real element tree
+    // Start with matching open element
+    open_ele()
+        .parse(input)
+        // Go on parsing
+        .and_then(
+            |(
+                rest,
+                Element {
+                    name: open_name,
+                    attributes,
+                    ..
+                },
+            )| {
+                // Match children
+                match one_or_more(right(space0(), tree)).parse(rest) {
+                    // Go on parsing
+                    Ok((rest, children)) => {
+                        // Match closing element
+                        right(space0(), close_ele()).parse(rest).and_then(
+                            |(
+                                rest,
+                                Element {
+                                    name: close_name, ..
+                                },
+                            )| {
+                                // Return an error if the closing element name doesn't correspond to the opening element's.
+                                if close_name == open_name {
+                                    // Final result
+                                    Ok((
+                                        rest,
+                                        Element {
+                                            name: open_name,
+                                            attributes,
+                                            children,
+                                        },
+                                    ))
+                                } else {
+                                    Err(rest)
+                                }
+                            },
+                        )
+                    }
+                    // TODO: redundant...
+                    Err(err) => Err(err),
+                }
+            },
+        )
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use indoc::indoc;
+    use pretty_assertions::assert_eq;
 
     #[test]
     fn parse_element_with_attributes() {
         assert_eq!(
-            xml_ele(r#"<single-element attribute="value"\>"#),
+            single_ele().parse(r#"<single-element attribute="value"\>"#),
             Ok((
                 "",
                 Element {
@@ -56,20 +134,52 @@ mod tests {
 
     #[test]
     fn parse_big_xml_tree() {
-        let str_tree = r#"
+        let str_tree = indoc!(
+            r#"
             <parent attr="I'm an attribute">
 
                 <child1\>
 
-                <child2 name="child" last_name="two"\>
-
-                <big_child name="big child"\>
+                <big_child name="big child">
                     <nested_child\>
                 <\big_child>
 
-            <\parent>
-        "#;
+                <child2 name="child" last_name="two"\>
 
-        assert_eq!(xml_tree(str_tree), Ok(("", Default::default(),)));
+            <\parent>"#
+        );
+
+        assert_eq!(
+            tree(str_tree),
+            Ok((
+                "",
+                Element {
+                    name: "parent".into(),
+                    attributes: vec![("attr".into(), "I'm an attribute".into())],
+                    children: vec![
+                        Element {
+                            name: "child1".into(),
+                            ..Default::default()
+                        },
+                        Element {
+                            name: "big_child".into(),
+                            attributes: vec![("name".into(), "big child".into())],
+                            children: vec![Element {
+                                name: "nested_child".into(),
+                                ..Default::default()
+                            }],
+                        },
+                        Element {
+                            name: "child2".into(),
+                            attributes: vec![
+                                ("name".into(), "child".into()),
+                                ("last_name".into(), "two".into())
+                            ],
+                            ..Default::default()
+                        }
+                    ],
+                }
+            ))
+        );
     }
 }
